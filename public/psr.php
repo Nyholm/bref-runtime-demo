d149e67d54e5a2d2e6bae7e0510d819f240112c9
Nyholm <tobias.nyholm@gmail.com>
2021-03-19 20:43:12 +0100

    Add PSR

---
+++ public/psr.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+return new class implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $name = $request->getQueryParams()['name'] ?? 'world';
+
+        return new Response(200, [], "Hello $name");
+    }
+};
