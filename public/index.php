7607d0dddc49e9834d06b80faf4b5135f2a4f083
Nyholm <tobias.nyholm@gmail.com>
2021-03-19 20:10:28 +0100

    more debug

--- public/index.php
+++ public/index.php
@@ -6,5 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     error_log(json_encode($context));
+    $e = new \RuntimeException('Foo');
+    error_log($e->getTraceAsString());
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
