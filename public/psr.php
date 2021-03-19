e089ce303c402451f5bc756b4f6e5da78ccfa561
Nyholm <tobias.nyholm@gmail.com>
2021-03-20 00:12:48 +0100

    Use runtime/bref

--- public/psr.php
+++ public/psr.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 return function () {
-    return new class implements RequestHandlerInterface {
+    return new class() implements RequestHandlerInterface {
         public function handle(ServerRequestInterface $request): ResponseInterface
         {
             $name = $request->getQueryParams()['name'] ?? 'world';
