e56b9e367fc79ab9f645b23ceeef8dbf2269ff9a
Nyholm <tobias.nyholm@gmail.com>
2021-03-26 09:44:12 +0100

    Adding a sleep

--- public/loop.php
+++ public/loop.php
@@ -9,6 +9,9 @@ class Handler implements \Bref\Event\Handler
     private static $count = 0;
     public function handle($event, Context $context)
     {
+        if (self::$count === 1) {
+            sleep(10);
+        }
         return 'Hello ' . self::$count++;
     }
 }
